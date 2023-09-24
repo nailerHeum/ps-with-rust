d36da88314f109fff88fa6cc94f1ac45448b48b7
Nailer <nailer@squarelab.co>
2023-09-24 20:44:57 +0900

    SOLVED leetcode 1768 Merge Strings Alternately

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,47 @@
-// baekjoon 11050 이항 계수 1
+// leetcode 1768 Merge Strings Alternately
 fn main() {
-    
-    let (a, b) = {
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input).unwrap();
-        let mut iter = input.trim().split_whitespace()
-            .map(|x| x.parse::<usize>().unwrap());
-        let a = iter.next().unwrap();
-        let b = iter.next().unwrap(); 
-        if a < b { (b, a) }
-        else { (a, b) }
-    };
-    let mut a_acc = 1;
-    let mut b_acc = 1;
-    for _b in 0..b {
-        a_acc *= a - _b;
-        b_acc *= _b + 1;
+  Solution::merge_alternately(String::from("abc"), String::from("pqr"));
+}
+
+#[derive(Debug)]
+struct Solution {}
+
+impl Solution {
+  pub fn merge_alternately(word1: String, word2: String) -> String {
+    let larger_len = if word1.len() > word2.len() { word1.len() } else { word2.len() };
+    let mut word1iter = word1.chars();
+    let mut word2iter = word2.chars();
+    let mut result: Vec<char> = Vec::new();
+    for _ in 0..larger_len {
+      match word1iter.next() {
+        Some(v) => result.push(v),
+        None => (),
+      }
+      match word2iter.next() {
+        Some(v) => result.push(v),
+        None => (),
+      }
     }
-    println!("{}", a_acc / b_acc);
+    
+    return result.iter().collect();
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::Solution;
+  #[test]
+  fn case_1() {
+    assert_eq!(Solution::merge_alternately(String::from("abc"), String::from("pqr")), String::from("apbqcr"));
+  }
+
+  #[test]
+  fn case_2() {
+    assert_eq!(Solution::merge_alternately(String::from("ab"), String::from("pqrs")), String::from("apbqrs"));
+  }
+
+  #[test]
+  fn case_3() {
+    assert_eq!(Solution::merge_alternately(String::from("abcd"), String::from("pq")), String::from("apbqcd"));
+  }
 }
