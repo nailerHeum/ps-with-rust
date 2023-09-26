a1f23e9bfccb342531e1f3631bda0890d97ed038
Nailer <nailer@squarelab.co>
2023-09-26 21:38:00 +0900

    SOLVED leetcode 1071 Greatest Common Divisor of Strings

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,71 @@
 // leetcode 1768 Merge Strings Alternately
 fn main() {
-  Solution::merge_alternately(String::from("abc"), String::from("pqr"));
+    Solution::gcd_of_strings(String::from("abc"), String::from("pqr"));
 }
 
 #[derive(Debug)]
 struct Solution {}
 
 impl Solution {
-  pub fn merge_alternately(word1: String, word2: String) -> String {
-    let larger_len = if word1.len() > word2.len() { word1.len() } else { word2.len() };
-    let mut word1iter = word1.chars();
-    let mut word2iter = word2.chars();
-    let mut result: Vec<char> = Vec::new();
-    for _ in 0..larger_len {
-      match word1iter.next() {
-        Some(v) => result.push(v),
-        None => (),
-      }
-      match word2iter.next() {
-        Some(v) => result.push(v),
-        None => (),
-      }
+    pub fn gcd_of_strings(str1: String, str2: String) -> String {
+        let (shorter_length, shorter_string, longer_length, longer_string) =
+            if str1.len() < str2.len() {
+                (str1.len(), str1, str2.len(), str2)
+            } else {
+                (str2.len(), str2, str1.len(), str1)
+            };
+
+        for i in 1..=shorter_length {
+            let short_rem = shorter_length.checked_rem(i).unwrap();
+            let short_div = shorter_length.checked_div(i).unwrap();
+            let long_rem = longer_length.checked_rem(short_div).unwrap();
+            if short_rem > 0 || long_rem > 0 {
+                continue;
+            }
+
+            let gcd_str = &shorter_string[0..short_div];
+            let multiply = longer_length.checked_div(short_div).unwrap();
+            if gcd_str.repeat(i) == shorter_string && gcd_str.repeat(multiply) == longer_string {
+                return String::from(gcd_str);
+            }
+        }
+
+        String::from("")
     }
-    
-    return result.iter().collect();
-  }
 }
 
 #[cfg(test)]
 mod tests {
-  use super::Solution;
-  #[test]
-  fn case_1() {
-    assert_eq!(Solution::merge_alternately(String::from("abc"), String::from("pqr")), String::from("apbqcr"));
-  }
+    use super::Solution;
+    #[test]
+    fn case_1() {
+        assert_eq!(
+            Solution::gcd_of_strings(String::from("ABC"), String::from("ABCABC")),
+            String::from("ABC")
+        );
+    }
 
-  #[test]
-  fn case_2() {
-    assert_eq!(Solution::merge_alternately(String::from("ab"), String::from("pqrs")), String::from("apbqrs"));
-  }
+    #[test]
+    fn case_2() {
+        assert_eq!(
+            Solution::gcd_of_strings(String::from("ABABAB"), String::from("ABAB")),
+            String::from("AB")
+        );
+    }
 
-  #[test]
-  fn case_3() {
-    assert_eq!(Solution::merge_alternately(String::from("abcd"), String::from("pq")), String::from("apbqcd"));
-  }
+    #[test]
+    fn case_3() {
+        assert_eq!(
+            Solution::gcd_of_strings(String::from("LEET"), String::from("CODE")),
+            String::from("")
+        );
+    }
+
+    #[test]
+    fn case_4() {
+        assert_eq!(
+            Solution::gcd_of_strings(String::from("AAAAAAAAA"), String::from("AAACCC")),
+            String::from("aa")
+        );
+    }
 }
