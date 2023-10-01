fd0ed5ce8e5c9c0f5c9696253449794c4b67de42
Nailer <nailer@squarelab.co>
2023-10-01 21:46:58 +0900

    SOLVED leetcode 605. Can Place Flowers

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
-// leetcode 1431. Kids With the Greatest Number of Candies
+// leetcode 605. Can Place Flowers
 fn main() {
-    Solution::kids_with_candies(vec![1, 2, 3], 1);
+    Solution::can_place_flowers(vec![1, 0, 1], 1);
 }
 
 #[derive(Debug)]
 struct Solution {}
-
 impl Solution {
-    pub fn kids_with_candies(candies: Vec<i32>, extra_candies: i32) -> Vec<bool> {
-        let max = *candies.iter().max().unwrap();
-        return candies.iter().map(|x| x + extra_candies >= max).collect();
+    pub fn can_place_flowers(flowerbed: Vec<i32>, n: i32) -> bool {
+        let mut capability = 0;
+
+        let mut zero_count = 1;
+        for flower in flowerbed {
+            if flower == 1 {
+                capability += (zero_count - 1) / 2;
+                zero_count = 0;
+            } else {
+                zero_count += 1;
+            }
+        }
+
+        if zero_count != 0 {
+            capability += zero_count / 2;
+        }
+        return capability >= n;
     }
 }
 
@@ -19,25 +32,11 @@ mod tests {
 
     #[test]
     fn case_1() {
-        assert_eq!(
-            Solution::kids_with_candies(vec![2, 3, 5, 1, 3], 3),
-            vec![true, true, true, false, true]
-        );
+        assert_eq!(Solution::can_place_flowers(vec![1, 0, 0, 0, 1], 1), true);
     }
 
     #[test]
     fn case_2() {
-        assert_eq!(
-            Solution::kids_with_candies(vec![4, 2, 1, 1, 2], 1),
-            vec![true, false, false, false, false]
-        );
-    }
-
-    #[test]
-    fn case_3() {
-        assert_eq!(
-            Solution::kids_with_candies(vec![12, 1, 12], 10),
-            vec![true, false, true]
-        );
+        assert_eq!(Solution::can_place_flowers(vec![1, 0, 0, 0, 1], 2), false);
     }
 }
