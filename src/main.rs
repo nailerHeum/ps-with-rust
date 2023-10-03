aaf605b19b8e864486ccbd7b6cff36fde6644d1b
Nailer <nailer@squarelab.co>
2023-10-03 14:12:31 +0900

    Solved leetcode 151. Reverse Words in a String

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,16 @@
-// leetcode 345. Reverse Vowels of a String
+// leetcode 151. Reverse Words in a String
 fn main() {
-    Solution::reverse_vowels("asdf".into());
+    Solution::reverse_words("asdf".into());
 }
 
 #[derive(Debug)]
 struct Solution {}
 impl Solution {
-    pub fn reverse_vowels(s: String) -> String {
-        let vowels: Vec<char> = vec!['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u'];
-        let mut collected_vowels: Vec<char> = s
-            .chars()
-            .filter(|c| vowels.iter().any(|x| x == c))
-            .collect();
-
-        s.chars()
-            .map(|c| {
-                if vowels.iter().any(|v| *v == c) {
-                    collected_vowels.pop().unwrap()
-                } else {
-                    c
-                }
-            })
-            .collect()
+    pub fn reverse_words(s: String) -> String {
+        s.split_ascii_whitespace()
+            .rev()
+            .collect::<Vec<&str>>()
+            .join(" ")
     }
 }
 
@@ -32,21 +21,24 @@ mod tests {
     #[test]
     fn case_1() {
         assert_eq!(
-            Solution::reverse_vowels("hello".into()),
-            String::from("holle")
+            Solution::reverse_words("the sky is blue".into()),
+            String::from("blue is sky the")
         );
     }
 
     #[test]
     fn case_2() {
         assert_eq!(
-            Solution::reverse_vowels("leetcode".into()),
-            String::from("leotcede")
+            Solution::reverse_words("  hello world  ".into()),
+            String::from("world hello")
         );
     }
 
     #[test]
     fn case_3() {
-        assert_eq!(Solution::reverse_vowels("aA".into()), String::from("Aa"));
+        assert_eq!(
+            Solution::reverse_words("a good   example".into()),
+            String::from("example good a")
+        );
     }
 }
