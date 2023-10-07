79af1e70143ba734a370f5e577400b8b4462641a
Nailer <nailer@squarelab.co>
2023-10-07 19:10:19 +0900

    SOLVED 238. Product of Array Except Self

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
-// leetcode 151. Reverse Words in a String
+// leetcode 238. Product of Array Except Self
 fn main() {
-    Solution::reverse_words("asdf".into());
+    Solution::product_except_self(vec![1, 2, 3, 4]);
 }
 
 #[derive(Debug)]
 struct Solution {}
+
 impl Solution {
-    pub fn reverse_words(s: String) -> String {
-        s.split_ascii_whitespace()
+    pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
+        let mut acc = 1;
+        let mut acc_rev = 1;
+
+        nums.iter()
+            .map(|x| {
+                let prev_product = acc.clone();
+                acc = acc * x;
+                return prev_product;
+            })
+            .collect::<Vec<i32>>()
+            .into_iter()
             .rev()
-            .collect::<Vec<&str>>()
-            .join(" ")
+            .enumerate()
+            .map(|(i, x)| {
+                let prev_product = acc_rev.clone();
+                acc_rev = acc_rev * nums[nums.len() - i - 1];
+                return prev_product * x;
+            })
+            .collect::<Vec<i32>>()
+            .into_iter()
+            .rev()
+            .collect()
     }
 }
 
@@ -21,24 +40,16 @@ mod tests {
     #[test]
     fn case_1() {
         assert_eq!(
-            Solution::reverse_words("the sky is blue".into()),
-            String::from("blue is sky the")
+            Solution::product_except_self(vec![1, 2, 3, 4]),
+            vec![24, 12, 8, 6]
         );
     }
 
     #[test]
     fn case_2() {
         assert_eq!(
-            Solution::reverse_words("  hello world  ".into()),
-            String::from("world hello")
-        );
-    }
-
-    #[test]
-    fn case_3() {
-        assert_eq!(
-            Solution::reverse_words("a good   example".into()),
-            String::from("example good a")
+            Solution::product_except_self(vec![-1, 1, 0, -3, 3]),
+            vec![0, 0, 9, 0, 0]
         );
     }
 }
