c3f33ddc84100bd6521f6a4baa005ffc5e775bb7
Nailer <nailer@squarelab.co>
2023-10-15 21:02:28 +0900

    SOLVED leetcode 334. Increasing Triplet Subsequence

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,26 @@
-// leetcode 238. Product of Array Except Self
+// leetcode 334. Increasing Triplet Subsequence
 fn main() {
-    Solution::product_except_self(vec![1, 2, 3, 4]);
+    Solution::increasing_triplet(vec![1, 2, 3, 4]);
 }
 
 #[derive(Debug)]
 struct Solution {}
-
 impl Solution {
-    pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-        let mut acc = 1;
-        let mut acc_rev = 1;
+    pub fn increasing_triplet(nums: Vec<i32>) -> bool {
+        let mut min_midium = i32::MAX;
+        let mut min_small = i32::MAX;
 
-        nums.iter()
-            .map(|x| {
-                let prev_product = acc.clone();
-                acc = acc * x;
-                return prev_product;
-            })
-            .collect::<Vec<i32>>()
-            .into_iter()
-            .rev()
-            .enumerate()
-            .map(|(i, x)| {
-                let prev_product = acc_rev.clone();
-                acc_rev = acc_rev * nums[nums.len() - i - 1];
-                return prev_product * x;
-            })
-            .collect::<Vec<i32>>()
-            .into_iter()
-            .rev()
-            .collect()
+        nums.into_iter().any(|x| {
+            if min_small >= x {
+                min_small = x;
+                false
+            } else if min_small < x && x <= min_midium {
+                min_midium = x;
+                false
+            } else {
+                true
+            }
+        })
     }
 }
 
@@ -39,17 +30,16 @@ mod tests {
 
     #[test]
     fn case_1() {
-        assert_eq!(
-            Solution::product_except_self(vec![1, 2, 3, 4]),
-            vec![24, 12, 8, 6]
-        );
+        assert_eq!(Solution::increasing_triplet(vec![1, 2, 3, 4, 5]), true);
     }
 
     #[test]
     fn case_2() {
-        assert_eq!(
-            Solution::product_except_self(vec![-1, 1, 0, -3, 3]),
-            vec![0, 0, 9, 0, 0]
-        );
+        assert_eq!(Solution::increasing_triplet(vec![5, 4, 3, 2, 1]), false);
+    }
+
+    #[test]
+    fn case_3() {
+        assert_eq!(Solution::increasing_triplet(vec![2, 1, 5, 0, 4, 6]), true);
     }
 }
